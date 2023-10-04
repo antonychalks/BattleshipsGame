c4de61e267cb02127ce85e691f8dd82876612735
antonychalks <>
2023-10-04 18:22:11 +0000

    Initial Commit

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import pprint
+
+def form_grid(x, y):
+    """
+    Used to create the grid at the size the user has given.
+    """
+    grid = ""
+    for _ in range(rows):
+        print("~" * columns)
+    
+    return grid
+
+
+grid = form_grid(5, 5)
+print(grid)
