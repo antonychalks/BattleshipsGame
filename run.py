8462046fa1c3573c50c22f2f1f3f1fc0c5e717bf
antonychalks <>
2023-10-04 19:21:40 +0000

    Defined a function to get a board size from the user

--- run.py
+++ run.py
@@ -1,18 +1,51 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
 import pprint
+from google.oauth2.service_account import Credentials
 
-def form_grid(x, y):
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Battleships')
+
+
+def get_size():
     """
-    Used to create the grid at the size the user has given.
+    Used to get the size of the board through user input
     """
-    grid = ""
-    for _ in range(rows):
-        print("~" * columns)
-    
-    return grid
+    print("Welcome to python battleships!")
+    while True:
+        x = input("How wide would you like the board to be? ")
+        y = input("How tall would you like the board to be? ")
+        if validate_input(x):
+            if validate_input(y):
+                correct = input(f"You have chosen a {x} x {y} board size! Is this correct? Y/N:")
+                if correct.lower() == 'y':
+                    break
+
+    xy = [x, y]
+    return xy
 
 
-grid = form_grid(5, 5)
-print(grid)
+def validate_input(num):
+    try:
+        if num.isnumeric():
+            return True
+        else:
+            raise ValueError(f"A whole number is required, you typed: {num}")
+    except ValueError as e:
+        print(f"Invalid data: {e}. Please try again.\n")
+        return False
+
+
+
+
+def main():
+    xy = get_size()
+
+main()
