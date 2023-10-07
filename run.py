813f0955bf35a75d9e25d734941d12b862596bdc
antonychalks <>
2023-10-07 12:29:02 +0000

    Defined the place_ships function to allow the user to place their ships.

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import string
 
+
 def get_board_size():
     """
     Get the size of the board through user input.
@@ -45,6 +46,15 @@ def validate_5(num):
         return False
 
 
+def validate_letter(letter, game_board):
+    if letter.upper() in string.ascii_uppercase[:5]:
+        return True
+    else:
+        print("Invalid letter input. Please enter a letter from A to E.")
+        return False
+
+
+
 def board(board_size):
     x, y = board_size
     game_board = [["~~" for _ in range(y)] for _ in range(x)]
@@ -61,14 +71,52 @@ def print_board(board):
         print(f"{i} {' '.join(row)}")
 
 
-def place_ships():
+def letter_to_number(letter):
+    return string.ascii_uppercase.index(letter.upper()) + 1
+
+
+def number_to_letter(number):
+    if 1 <= number <= 26:
+        return string.ascii_uppercase[number - 1]
+    else:
+        raise ValueError("Number must be between 1 and 26")
+
+
+def ship_qty():
+    ship = input("How many Ships should each player have?\n")
+    return int(ship)
+
+
+def place_ships(xy, game_board, qty):
     print("Now you need to place your ships")
-    
+    ships_remain = qty
+    max_y = number_to_letter(xy[1])
+
+    while ships_remain > 0:
+        print(f"You have {ships_remain} ships remaining.")
+        y_letter = input(f"Please input the Y axis of the ship you would like to place (A to {max_y}): ")
+        x = int(input(f"Please input the X axis of the ship you would like to place (1 to {xy[0]}): "))
+        
+        y = letter_to_number(y_letter)
+        if 1 <= x <= len(game_board) and y <= xy[1]:
+            if game_board[x - 1][y - 1] == "~~":
+                game_board[x - 1][y - 1] = "SS"
+                print_board(game_board)
+                ships_remain -= 1
+            else:
+                print("Sorry, a ship is already placed at those coordinates. Try again.")
+        else:
+            print("Invalid coordinates. Please enter valid coordinates within the board size.")
+
+    print("All ships have been placed.")
 
 
 def main():
     xy = get_board_size()
     game_board = board(xy)
-    place_ships()
+    num_ships = ship_qty()  # Call the ship_qty function to get the number of ships
+    place_ships(xy, game_board, num_ships)  # Pass the number of ships to place_ships
+
 
 main()
+
