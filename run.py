2115172600e9bec283f5e46239b0ba1601d60789
antonychalks <>
2023-10-10 14:25:43 +0000

    Ran through the game and tested everything, noted the bugs in bugs.txt, then resolved

--- run.py
+++ run.py
@@ -20,74 +20,144 @@ def is_testing():
         is_testing()
 
 
-def get_board_size(testing):
+def get_board_size():
     """
     Get the size of the board through user input.
     """
     print("Welcome to Python Battleships!")
+    
     while True:
-        x = input("How tall would you like the board to be? ")
-        if validate_int(x, testing) and validate_5(x, testing):
-            break
+        xy = input(f"How tall and wide would you like the board to be? (Min 6, Max 26)\nPlease enter in the following format: 'X x Y'\nIf you would like a random board, type 'random'.\n")
+        if validate_board_size(xy):
+            if xy.lower() == "random":
+                x = random.randint(6, 26)
+                y = random.randint(6, 26)
+                break
+            else:
+                xy = xy.split(" x ")
+                x = int(xy[0])
+                y = int(xy[1])
+                if validate_int(x) and validate_board(x):
+                    if validate_int(y) and validate_board(y):
+                        break
+                    else:
+                        print("y value invalid, please try again")
+                else:
+                    print("x value invalid, please try again")
+            print("Invalid input, please try again.")
+        else:
+            print("Invalid format. Input should be X x Y, with a space between each character and two whole numbers for the X and Y")
 
     while True:
-        y = input("How wide would you like the board to be? ")
-        if validate_int(y, testing) and validate_5(y, testing):
+        correct = input(f"You have chosen a {x} x {y} board size! Is this correct? (Y/N): ").lower()
+        if correct == 'y':
+            return int(x), int(y)
             break
+        elif correct == 'n':
+            print("Please re-enter the board size.")
+            return get_board_size()
+            break
+        else:
+            print("Invalid input")
 
-    correct = input(f"You have chosen a {x} x {y} board size! Is this correct? (Y/N): ").lower()
-    if correct == 'y':
-        return int(x), int(y)
-    else:
-        print("Please re-enter the board size.")
-        return get_board_size(testing)
+
+def validate_board_size(value):
+
+    try:
+        if len(value) > 4:
+            return True
+        else:
+            raise ValueError("You have not used the correct format, You need to type the first number followed by ' x ' followed by your second number.")
+    except ValueError as e:
+        print(f"Invalid data: {e}. Please try again.")
+        return False
 
 
-def validate_int(num, testing):
-    if is_testing:
-        print("Skipping Validation: validate_int")
+def validate_int(num):
+    if testing == True:
+        print("TEST: Skipping Validation: validate_int")
         return True
     else:
         try:
-            if num.isnumeric():
+            num = int(num)
+            if isinstance(num, int):
                 return True
             else:
                 raise ValueError(f"A whole number is required, you typed: {num}")
+            if num > xy[0]:
+                raise ValueError(f"Only one Character is allowed.")
+            else:
+                return True
         except ValueError as e:
             print(f"Invalid data: {e}. Please try again.")
             return False
 
 
-def validate_5(num, testing):
-    if is_testing:
-        print("Skipping Validation: validate_5")
+def validate_board(num):
+    if testing == True:
+        print("TEST: Skipping Validation: validate_board")
         return True
     else:
         try:
-            if int(num) >= 6:
+            num = int(num)
+            if 5 < num < 27:
                 return True
             else:
-                raise ValueError(f"A number larger than 5 is required. You typed {num}")
+                raise ValueError(f"A number larger than 5 and smaller than 27 is required. You typed {num}")
         except ValueError as e:
             print(f"Invalid data: {e}. Please try again.")
             return False
 
 
-def validate_letter(letter, game_board, testing):
-    if is_testing:
-        print("Skipping Validation: validate_letter")
+def validate_letter(letter, game_board):
+    if testing == True:
+        print("TEST: Skipping Validation: validate_letter")
         return True
     else:
-        if letter.upper() in string.ascii_uppercase[:5]:
-            return True
-        else:
-            print("Invalid letter input. Please enter a letter from A to E.")
+        try:
+            if letter_to_number(letter.upper()) > xy[1]:
+                raise ValueError(f"Invalid letter input.")
+                return False
+            else:
+                return True
+            if type(letter) is str:
+                return True
+            else:
+                raise ValueError("Your input was not a letter.")
+        except ValueError as e:
+            print(f"Invalid data: {e}. Please try again with a letter from A to {number_to_letter(xy[1])}.")
             return False
 
 
+def validate_ship_qty(ship):
+    if testing:
+        print("TEST: Skipping Validation: validate_ship_qty")
+        return True
+    else:
+        try:
+            ship = int(ship)
+            area = xy[0] * xy[1]
+            if area <= ship:
+                raise ValueError(f"The number of ships entered ({ship}) is more than the available squares ({area}).")
+            
+            if area * 0.75 <= ship:
+                print(f"WARNING: The number of ships entered ({ship}) is more than 75% of the available squares ({area}).")
+                while True:
+                    proceed = input("Do you wish to proceed? Y/N: ")
+                    if proceed == "Y":
+                        return True
+                    elif proceed == "N":
+                        return False
+            else:
+                return True  # Move this line outside of the else block
+        except ValueError as e:
+            print(f"Invalid data: {e}. Please try again with a number lower than {area}.")
+            return False
+
+                    
 def board(board_size, is_user):
     x, y = board_size
-    game_board = [["~~" for _ in range(y)] for _ in range(x)]
+    game_board = [["~~" for num in range(y)] for num in range(x)]
     if is_user == "user" or is_user == "computer":
         print_board(game_board, is_user)
     return game_board
@@ -102,7 +172,10 @@ def print_board(board, is_user):
     header_str = "  " + "  ".join(column_headers)
     print(header_str)
     for i, row in enumerate(board, 1):
-        print(f"{i} {' '.join(row)}")
+        if i < 10:
+            print(f"{i}  {' '.join(row)}")
+        else:
+            print(f"{i} {' '.join(row)}")
 
 
 def letter_to_number(letter):
@@ -117,21 +190,37 @@ def number_to_letter(number):
 
 
 def ship_qty():
-    ship = input("How many Ships should each player have?\n")
+    while True:
+        print("How many Ships should each player have?\n")
+        ship = input("You can also type random for a random amount: ")
+        if ship.lower() == "random":
+            ship = random.randint(1, xy[1])
+            if validate_ship_qty(ship) and validate_int(ship):
+                break
+        if validate_ship_qty(ship) and validate_int(ship):
+            break
     return int(ship)
+
+
+def select_coords():
+    max_y = number_to_letter(xy[1])
+    yx = input(f"Please input the X and Y co-ordinates of the square you would like to select. \nFor example: D12\nPlease do not exceed {max_y}{xy[0]}: ")
+    y_letter = yx[0]
+    x = int(yx[1:3])
+    y = letter_to_number(y_letter)
+    return x, y
 
 
 def place_ships(xy, game_board, qty, is_user):
     ships_remain = qty
-    max_y = number_to_letter(xy[1])
 
     while ships_remain > 0:
         if is_user == "user":
             print("Now you need to place your ships")
             print(f"You have {ships_remain} ships remaining.")
-            y_letter = input(f"Please input the Y axis of the ship you would like to place (A to {max_y}): ")
-            x = int(input(f"Please input the X axis of the ship you would like to place (1 to {xy[0]}): "))
-            y = letter_to_number(y_letter)
+            print("Where would you like to place your ship?")
+            coords = select_coords()
+            x, y = coords
         else:
             print("Randomising computer ships...")
             x = random.randint(1, xy[0])
@@ -151,9 +240,7 @@ def place_ships(xy, game_board, qty, is_user):
     print("All ships have been placed.")
 
 
-def play_game(game_board_blank, game_board_user, game_board_computer, num_ships, xy):
-    
-        
+def play_game(game_board_blank, game_board_user, game_board_computer, num_ships, xy):       
     def user_first(num_ships):
         user_ships = num_ships
         comp_ships = num_ships
@@ -211,21 +298,21 @@ def play_game(game_board_blank, game_board_user, game_board_computer, num_ships,
         elif user_ships == 0:
             print("YOU LOSE! Computer wins!")
 
-    print("Who goes first?")
-    first_turn = input("User, Computer, Random: ")
-
-    if first_turn.lower() == "user":
-        user_first(num_ships)
-    elif first_turn.lower() == "computer":
-        comp_first(num_ships)
-    elif first_turn.lower(num_ships) == "random":
-        random_start = random.randint(1, 2)
-        if random_start == 1:
+    while True:
+        print("Who goes first?")
+        first_turn = input("User, Computer, Random: ")
+        if first_turn.lower() == "user":
             user_first(num_ships)
-        else:
+        elif first_turn.lower() == "computer":
             comp_first(num_ships)
-    else:
-        print("Invalid input. Please try again:")
+        elif first_turn.lower(num_ships) == "random":
+            random_start = random.randint(1, 2)
+            if random_start == 1:
+                user_first(num_ships)
+            else:
+                comp_first(num_ships)  
+        else:
+            print("Invalid input. Please try again:")
 
 
 
@@ -236,19 +323,19 @@ def take_turn(game_board_turn, game_board_spectator, game_board_blank, is_user, 
         print_board(game_board_blank, "blank")
         print_board(game_board_turn, "user")
         print("Now you need to chose where to launch missile")
-        y_letter = input(f"Please input the Y axis of the position you would like to target (A to {max_y}): ")
-        x = int(input(f"Please input the X axis of the position you would like to target (1 to {xy[0]}): "))
-        y = letter_to_number(y_letter)
+        coords = select_coords()
+        x, y = coords
         print(number_to_letter(y)+str(x))
     else:
         print("Computer taking their turn")
         x = random.randint(1, xy[0])
         y = random.randint(1, xy[1])
         print(number_to_letter(y)+str(x))
-            
-    if 1 <= x <= len(game_board_spectator) and y <= xy[1]:
+    
+    if 1 <= int(x) <= len(game_board_spectator) and y <= xy[1]:
         if game_board_spectator[x - 1][y - 1] == "xx" or game_board_spectator[x - 1][y - 1] == "><":
             print("Co-ordinates already selected!")
+            take_turn(game_board_turn, game_board_spectator, game_board_blank, is_user, xy)
         else:
             if game_board_spectator[x - 1][y - 1] == "SS":
                 game_board_spectator[x - 1][y - 1] = "><"
@@ -263,13 +350,16 @@ def take_turn(game_board_turn, game_board_spectator, game_board_blank, is_user, 
                     game_board_blank[x - 1][y - 1] = "xx"
     else:
         print("Invalid coordinates. Please enter valid coordinates within the board size.")
+        take_turn(game_board_turn, game_board_spectator, game_board_blank, is_user, xy)
     print("Changing player...")
     time.sleep(2)
 
 
 def main():
+    global xy
+    global testing
     testing = is_testing()
-    xy = get_board_size(testing)
+    xy = get_board_size()
     game_board_blank = board(xy, "blank")
     game_board_user = board(xy, "user")
     game_board_computer = board(xy, "computer")
